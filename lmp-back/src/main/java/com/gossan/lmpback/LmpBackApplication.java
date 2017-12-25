b2597178f78383ab1815a1b514ef11ee69e56d1f
adjagossan <adjagossan@gmail.com>
2017-12-25 14:46:14 +0100

    first commit

---
+++ lmp-back/src/main/java/com/gossan/lmpback/LmpBackApplication.java
@@ -0,0 +1,12 @@
+package com.gossan.lmpback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmpBackApplication.class, args);
+	}
+}
